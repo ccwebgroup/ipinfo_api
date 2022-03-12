2975e87ffb7ccb3cd021a4657d68efba5ec2d7f4
ccwebgroup <ccwebgroup@gmail.com>
2022-03-13 00:42:25 +0800

    Added ip who is API

--- index.js
+++ index.js
@@ -73,4 +73,76 @@ app.get("/ranges/:domain", async (req, res) => {
   }
 });
 
+//Search Net by Net ID
+app.get("/whois/net/:net_id", async (req, res) => {
+    const { token } = req.query;
+    const net_id = req.params.net_id;
+    try {
+      const result = await axios.get(`${baseUrl}whois/net/${net_id}?token=${token}`);
+      res.json(result.data);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
+//Search Net by IP or IP Range
+app.get("/whois/net/:ip", async (req, res) => {
+    const { token } = req.query;
+    const ip = req.params.ip;
+    try {
+      const result = await axios.get(`${baseUrl}whois/net/${ip}?token=${token}`);
+      res.json(result.data);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
+//Search Net by domain
+app.get("/whois/net/:domain", async (req, res) => {
+    const { token } = req.query;
+    const domain = req.params.domain;
+    try {
+      const result = await axios.get(`${baseUrl}whois/net/${domain}?token=${token}`);
+      res.json(result.data);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
+//Search Net records by ASN
+app.get("/whois/net/:asn", async (req, res) => {
+    const { token } = req.query;
+    const asn = req.params.asn;
+    try {
+      const result = await axios.get(`${baseUrl}whois/net/${asn}?token=${token}`);
+      res.json(result.data);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
+//Search Org records by Org ID
+app.get("/whois/org/:id", async (req, res) => {
+    const { token } = req.query;
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`${baseUrl}whois/org/${id}?token=${token}`);
+      res.json(result.data);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
+//Search POC records by POC ID
+app.get("/whois/poc/:id", async (req, res) => {
+    const { token } = req.query;
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`${baseUrl}whois/poc/${id}?token=${token}`);
+      res.json(result.data);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
 app.listen(PORT, () => console.log(`Server is runninng in port ${PORT}`));
